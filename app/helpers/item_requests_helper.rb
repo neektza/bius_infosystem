1b00bf1fb0f1bc43948273e6cfd95c9b661c163a
neektza <neektza@lappy486.localdomain>
2010-10-24 23:48:22 +0200

    removed stuff

--- app/helpers/item_requests_helper.rb
+++
@@ -1,9 +0,0 @@
-module ItemRequestsHelper
-  def display_action(action)
-    rs = case action
-      when ItemRequest::Action::ASSIGNMENT then "Zaduzenje"
-      when ItemRequest::Action::ASSIGNMENT then "Razduzenje"
-    end
-    return rs
-  end
-end
