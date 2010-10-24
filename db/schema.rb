7011992968765935d6ec5b3b5d55c4028e42d9b3
neektza <neektza@lappy486.localdomain>
2010-10-24 23:53:08 +0200

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -213,16 +213,6 @@ ActiveRecord::Schema.define(:version => 20100907154257) do
     t.datetime "updated_at"
   end
 
-  create_table "sessions", :force => true do |t|
-    t.string   "session_id", :null => false
-    t.text     "data"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
-  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
-
   create_table "tags", :force => true do |t|
     t.string   "title",       :null => false
     t.text     "description"
@@ -242,5 +232,15 @@ ActiveRecord::Schema.define(:version => 20100907154257) do
     t.string   "io",              :limit => 8
     t.boolean  "is_secret"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
 end
