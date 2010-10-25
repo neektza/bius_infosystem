7b343b962cbfbcec992386697b4a6861969f4bd7
neektza <neektza@lappy486.localdomain>
2010-10-25 02:35:20 +0200

    fakof

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,8 +1,8 @@
-class ItemLoan < ActiveRecord::Base
+class Loan < ActiveRecord::Base
   belongs_to :borrower, :class_name => "Member", :foreign_key => "borrower_id"
   belongs_to :lender, :class_name => "Member", :foreign_key => "lender_id"
   
-  belongs_to :item
+  belongs_to :loanable
   
   def self.period
     self.date_to - self.date_from
