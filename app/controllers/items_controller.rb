551333567f2928d0f07278c64e4f59571e7e2b97
neektza <neektza@gmail.com>
2010-11-03 00:55:03 +0100

    bo?

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,7 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def edit
--- app/helpers/item_loans_helper.rb
+++ app/helpers/item_loans_helper.rb
@@ -1,5 +1,4 @@
 module ItemLoansHelper
-
   def item_loans_form(form)
     render :partial => 'form', :locals => {:f => form}
   end
